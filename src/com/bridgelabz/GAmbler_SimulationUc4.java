86df05a73d9cb6b75432e873ec51dfe872352ce7
sameers999 <syedmahammadsameer999>
2022-04-10 15:13:57 +0530

    Gambler Simulation

---
+++ src/com/bridgelabz/GAmbler_SimulationUc4.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class GAmbler_SimulationUc4 {
+	public class gamblerSimulator {
+
+		static int EVERY_DAY_STAKE = 100;
+		static int BET_PER_GEME = 1;
+		static int totalWon = 1;
+		static int totalLoss = 0;
+
+		public static void main(String[] args) {
+			Scanner s = new Scanner(System.in);
+			int cash = EVERY_DAY_STAKE;
+			s.close();
+			while (cash > 50 && cash < 150) {
+				int gamePlay = (Math.random() <= 0.5) ? 1 : 2;
+				switch(gamePlay) {
+					case 1:
+						cash = cash + BET_PER_GEME;
+						break;
+					case 2:
+						cash = cash - BET_PER_GEME;
+						break;
+				}
+				if (gamePlay == 1) {
+					System.out.println("Gambler Won");
+				}
+				else {
+					System.out.println("Gambler Lost");
+				}
+			}
+			if (cash == 150 || cash == 50) {
+				System.out.println("Player Quits!");
+			}
+		}
+	}
+}
